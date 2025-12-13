f9f35e19e993ea8030e4d58a0d478fe2da647ab1
Luiza Martins <luiza.martins@asaas.com.br>
2025-12-13 12:59:39 -0300

    ESTHER-01 Comenta worst e first fit

--- classes/alg_impl/FirstFit.py
+++ classes/alg_impl/FirstFit.py
@@ -3,13 +3,30 @@ from classes.Byte import Byte
 
 
 class FirstFit(Algoritmo):
+    """
+        Implementação do algoritmo First Fit para alocação dinâmica de memória.
+
+        A estratégia consiste em percorrer a memória e alocar o bloco no primeiro
+        espaço livre contíguo que seja suficientemente grande para atender à
+        requisição.
+    """
 
     def procura_bloco_livre(self, mem: list[Byte], tam_bloco: int, id: int) -> int:
+        """
+            Procura o primeiro bloco livre e realiza a alocação imediatamente após encontrá-lo.
+
+            :param mem: Lista que representa a memória física simulada.
+            :param tam_bloco: Tamanho do bloco a ser alocado.
+            :param id: Identificador único do bloco alocado.
+            :return: Índice inicial do bloco alocado ou -1 caso não exista espaço.
+        """
         i = 0
+        # Percorre a memória até encontrar um bloco livre contíguo viável
         while i < len(mem):
             if mem[i].idx_ini > -1:
                 i += mem[i].tam
             else:
+                # Identifica o tamanho do bloco livre a partir da posição atual
                 idx_ini = i
                 tam_possivel = 1
                 while idx_ini + tam_possivel < len(mem) and mem[idx_ini + tam_possivel].id == -1:
